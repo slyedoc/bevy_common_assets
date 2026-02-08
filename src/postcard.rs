f81f6c79b5bedab0b669b9c18323a8893ca6936e
Patrick Towles <james.towles@gmail.com>
2026-02-08 01:00:10 -0500

    fix: update SavedAsset to use two lifetime parameters

--- src/postcard.rs
+++ src/postcard.rs
@@ -107,7 +107,7 @@ impl<A: Asset + for<'de> Deserialize<'de> + Serialize> AssetSaver for PostcardAs
     async fn save(
         &self,
         writer: &mut bevy_asset::io::Writer,
-        asset: bevy_asset::saver::SavedAsset<'_, Self::Asset>,
+        asset: bevy_asset::saver::SavedAsset<'_, '_, Self::Asset>,
         _settings: &Self::Settings,
     ) -> Result<<Self::OutputLoader as AssetLoader>::Settings, Self::Error> {
         let bytes = to_stdvec(&asset.get())?;
